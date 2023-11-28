7b304d199ec3d89f53b738315be9e0b30ac95fcc
Julio <juliobrito.tec@gmail.com>
2023-11-27 21:39:47 -0300

    add: services and adapters

---
+++ src/main/java/com/julio/emailservice/application/EmailSenderService.java
@@ -0,0 +1,21 @@
+package com.julio.emailservice.application;
+
+import com.julio.emailservice.adapters.EmailSenderGateway;
+import com.julio.emailservice.core.EmailSenderUseCase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailSenderService implements EmailSenderUseCase {
+    private final EmailSenderGateway emailSenderGateway;
+
+    @Autowired
+    public EmailSenderService(EmailSenderGateway emailGateway) {
+        this.emailSenderGateway = emailGateway;
+    }
+
+    @Override
+    public void sendEmail(String to, String subject, String body) {
+        this.emailSenderGateway.sendEmail(to, subject, body);
+    }
+}
